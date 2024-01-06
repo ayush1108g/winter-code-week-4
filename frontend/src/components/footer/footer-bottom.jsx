59791bc4f2a3dcb3526aed3dbbe581f10b680835
DivyaKumar5161 <divyakumar768800@gmail.com>
2024-01-06 23:41:23 +0530

    card modification

--- frontend/src/components/footer/footer-bottom.jsx
+++ frontend/src/components/footer/footer-bottom.jsx
@@ -8,7 +8,7 @@ export default function FooterBottom() {
         <img src="https://codewithsadee.github.io/anon-ecommerce-website/assets/images/payment.png" alt="payment method"
           className="payment-img" />
         <p className="copyright">
-          Copyright &copy; <li >EduTech</li> all rights reserved.
+          Copyright &copy; <li className="">EduTech</li> all rights reserved.
         </p>
       </div>
     </div>);
