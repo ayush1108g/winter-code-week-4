2d3d592c4e59a4695e0faef434fab76aab1f6f3c
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-01-06 12:11:21 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,91 @@
-import "./App.css";
+import classes from "./App.module.css";
 
-function App() {
-  return <div className="App">EDU - HUB</div>;
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes, useLocation, HashRouter } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import Errorpage from "./pages/Errorpage";
+import TeamPage from "./pages/teamPage";
+import ForgotPassPage from "./pages/ForgotPass/ForgotPassPage";
+import ForgotPassIDPage from "./pages/ForgotPass/ForgotPassIDPage";
+import ForgotPassConfirmPage from "./pages/ForgotPass/ForgotPassConfirmPage";
+
+export const ToLink = "https://ecommerce-web-lwxy.onrender.com";
+// export const ToLink = "http://127.0.0.1:8000";
+export const FromLink = "https://swiftshop-ecommerce.netlify.app/#/";
+
+function RoutesWithAnimation() {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.key}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/forgotpassword" element={<ForgotPassPage />} />
+        <Route
+          path="/login/forgotpassword/:id"
+          element={<ForgotPassIDPage />}
+        />
+        <Route
+          path="/login/forgotpassword/:id/confirm"
+          element={<ForgotPassConfirmPage />}
+        />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Errorpage />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
+function App() {
+  const screenWidth = window.innerWidth;
+  return (
+    <HashRouter>
+      <div>
+        <div
+          className="d-none d-lg-block"
+          style={{
+            position: "fixed",
+            width: "18rem",
+            backgroundColor: "black",
+          }}
+        >
+          <SideBar />
+        </div>
+        <div
+          className={`h1 d-flex justify-content-center ${classes.gradient}`}
+          style={{
+            width: "100vw",
+            height: "auto",
+            padding: "10px",
+            zIndex: 10,
+          }}
+        >
+          {" "}
+          EduShop
+        </div>
+        <div className="navbar-expand-lg d-lg-none">
+          <Navbar />
+        </div>
+        <div
+          className=""
+          style={{
+            marginLeft: screenWidth > 992 ? "24rem" : "2rem",
+            height: "100dvh",
+            overflowY: "auto",
+          }}
+        >
+          <RoutesWithAnimation />
+          <div style={{ width: "100%", height: "15vh" }}></div>
+        </div>
+      </div>
+    </HashRouter>
+  );
+}
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import classes from "./Navbar.module.css";
+import { useNavigate } from "react-router";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = (params) => {
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.clear();
+    navigate('/login');
+  }
+  return (<>
+    <div className={classes.navbar}>
+      <div className="dropdown" >
+        <button className="btn"
+          type="button" id="dropdownMenuButton" data-toggle="dropdown" >
+          <div className="h2 d-flex" style={{ flexDirection: 'row' }}> <span ><GiHamburgerMenu /></span><span>&nbsp;&nbsp;Menu</span></div>
+        </button>
+        <div className="dropdown-menu bg-light" aria-labelledby="dropdownMenuButton">
+          <li onClick={() => { navigate('/') }} className="dropdown-item" >Welcome</li>
+          <li className="dropdown-item" onClick={() => { navigate('/patientRegistration') }}> Patient Registration</li>
+          <li className="dropdown-item" onClick={() => { navigate('/patientList') }}>Patient List</li>
+          <li className="dropdown-item" onClick={() => { navigate('/hospitalRoomStatus') }}>Hospital Room Status </li>
+          <li className="dropdown-item" onClick={() => { navigate('/payment') }}>Payment</li>
+          <li className="dropdown-item" onClick={() => { navigate('/employeeRegistration') }}>  Employee Registration</li>
+          <li className="dropdown-item" onClick={() => { navigate('/employeeList') }}>  Employee List </li>
+          <li className="dropdown-item" onClick={() => { navigate('/team') }}>Team</li>
+          <li className="dropdown-item" onClick={logoutHandler} > LOGOUT</li>
+        </div>
+      </div>
+    </div >
+  </>
+  );
+};
+export default Navbar;
